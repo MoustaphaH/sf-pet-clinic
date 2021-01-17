b36101071f1237b5131939770d7528cec361b2da
MoustaphaH <mmuhammedzaki@gmail.com>
2021-01-17 22:25:57 +0200

    Initial Project Add

---
+++ src/main/java/mdev/springframework/sfpetclinic/SfPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mdev.springframework.sfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mdev/springframework/sfpetclinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mdev.springframework.sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
