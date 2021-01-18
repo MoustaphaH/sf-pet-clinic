4e5f3e7b892a3130bcd15d853098d6f66e76c487
MoustaphaH <mmuhammedzaki@gmail.com>
2021-01-18 12:58:46 +0200

    Owner Page & Controller Added. CLoses #12.

--- pet-clinic-web/src/main/java/mdev/springframework/sfpetclinic/Controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/mdev/springframework/sfpetclinic/Controllers/OwnerController.java
@@ -3,10 +3,12 @@ package mdev.springframework.sfpetclinic.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
-public class VetController {
-    @RequestMapping({"vets", "vets/index", "vets/index.html"})
-    public String listViews(){
-        return "Vets/index";
+public class OwnerController {
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listOwners(){
+        return "Owners/index";
     }
 }
