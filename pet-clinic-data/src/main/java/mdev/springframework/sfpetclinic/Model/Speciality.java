6f74741b4072fbc1dd90f00398c688177f20de30
MoustaphaH <mmuhammedzaki@gmail.com>
2021-01-22 19:32:56 +0200

    Speciality added & added to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/mdev/springframework/sfpetclinic/Model/Speciality.java
@@ -0,0 +1,13 @@
+package mdev.springframework.sfpetclinic.Model;
+
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/mdev/springframework/sfpetclinic/Model/Vet.java
+++ pet-clinic-data/src/main/java/mdev/springframework/sfpetclinic/Model/Vet.java
@@ -1,4 +1,15 @@
 package mdev.springframework.sfpetclinic.Model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
